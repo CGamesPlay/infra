83cac3be828574f9218db826329ca7fac63a6c5d
Ryan Patterson <cgamesplay@cgamesplay.com>
2022-08-23 12:19:02 +0000

    Private server: Start to build auto-shutdown script

--- private-server/template-user-data.py
+++ private-server/template-user-data.py
@@ -4,6 +4,19 @@ import yaml
 import requests
 import subprocess
 import os
+import gzip
+
+def file(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+def write_file(path, content, permissions):
+    compressed = gzip.compress(content.encode('utf-8'))
+    encoding = None
+    if len(compressed) < len(content):
+        content = compressed
+        encoding = 'gzip'
+    return { 'path': path, 'content': content, 'permissions': permissions, 'encoding': encoding }
 
 r = requests.get('https://vault.service.consul:8200/v1/pki/ca/pem')
 r.raise_for_status()
@@ -27,10 +40,15 @@ user_data = {
         'enabled': True,
     },
     'package_update': True,
-    'packages': ['docker.io'],
+    'packages': ['docker.io', 'hcloud-cli', 'python3', 'python3-pip'],
     'ca_certs': {
         'trusted': [ca_cert]
     },
+    'runcmd': [],
+    'write_files': [
+        write_file('/usr/local/bin/ps-auto-shutdown', file('ps-auto-shutdown'), '0755'),
+        write_file('/usr/local/bin/ps-resize-drive', file('ps-resize-drive'), '0755'),
+    ],
 }
 
 print('#cloud-config')
