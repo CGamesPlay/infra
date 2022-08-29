7efc65b8bcf57b99c071019a106f99e45514c954
Ryan Patterson <cgamesplay@cgamesplay.com>
2022-08-29 11:47:06 +0000

    Improve deploy script

--- scripts/levant.sh
+++ scripts/levant.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-set -ueio pipefail
+set -ueo pipefail
 shopt -s extglob
 
 operation=$1
 shift
+n_error=0
 
 JOBS=${@:-nomad/!(*.disabled)}
 for job in $JOBS; do
     job=$(basename "$job")
     echo $job
-    (
-        cd "nomad/$job"
-        levant $operation -ignore-no-changes
-    )
+    cd "nomad/$job"
+    if ! levant $operation -ignore-no-changes; then
+        ((++n_error))
+    fi
+    cd ../..
     echo
 done
+
+exit $n_error
