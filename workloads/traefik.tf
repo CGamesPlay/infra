404f363e8f1e9e919f42e1b1935c9354a8f82eac
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-05-30 18:13:45 +0800

    Use HCL instead of heredoc yaml

--- workloads/traefik.tf
+++ workloads/traefik.tf
@@ -8,40 +8,48 @@ resource "kubernetes_manifest" "traefik_config" {
     }
     spec = {
       // https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
-      valuesContent = <<-EOF
-        image:
-          repository: docker.io/library/traefik
-          tag: 3.3.5
-        ports:
-          web:
-            redirections:
-              entryPoint:
-                to: websecure
-                scheme: https
-                permanent: true
-          websecure:
-            asDefault: true
-          metrics: null
-        ingressRoute:
-          dashboard:
-            enabled: true
-            matchRule: "Host(`traefik.${var.domain}`)"
-            entryPoints:
-            - websecure
-            middlewares:
-            - name: ${local.auth_middleware}
-        providers:
-          kubernetesCRD:
-            allowCrossNamespace: true
-        metrics:
-          prometheus: null
-        globalArguments: null
-        logs:
-          general:
-            level: ${var.verbose ? "DEBUG" : "INFO"}
-          access:
-            enabled: true
-        EOF
+      valuesContent = yamlencode({
+        ports = {
+          web = {
+            redirections = {
+              entryPoint = {
+                to        = "websecure"
+                scheme    = "https"
+                permanent = true
+              }
+            }
+          }
+          websecure = {
+            asDefault = true
+          }
+          metrics = null
+        }
+        ingressRoute = {
+          dashboard = {
+            enabled     = true
+            matchRule   = "Host(`traefik.${var.domain}`)"
+            entryPoints = ["websecure"]
+            middlewares = [{ name = local.auth_middleware }]
+          }
+        }
+        providers = {
+          kubernetesCRD = {
+            allowCrossNamespace = true
+          }
+        }
+        metrics = {
+          prometheus = null
+        }
+        globalArguments = null
+        logs = {
+          general = {
+            level = var.verbose ? "DEBUG" : "INFO"
+          }
+          access = {
+            enabled = true
+          }
+        }
+      })
     }
   }
 }
